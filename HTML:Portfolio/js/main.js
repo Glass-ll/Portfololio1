efde04adab0e1d911102e7a719c6f324d7b8b163
Glass-ll <106920683+Glass-ll@users.noreply.github.com>
2022-08-10 21:19:47 +0900

    New

---
+++ HTML:Portfolio/js/main.js
@@ -0,0 +1,7 @@
+$('#page-link a[href*="#"]').click(function () {
+	var elmHash = $(this).attr('href'); 
+	var pos = $(elmHash).offset().top;	
+	$('body,html').animate({scrollTop: pos}, 500); 
+	return false;
+});
+
